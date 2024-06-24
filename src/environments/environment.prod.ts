4951d36539f83b614e368738008b82bd9cb97449
Mairton Leal <mairton.rodrigues12@hotmail.com>
2024-06-23 21:50:50 -0300

    environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyCDiMv0qP3qKo4OdqwCKkbMuaR6UGfMzo8",
-    authDomain: "sollowfirebase-4f1da.firebaseapp.com",
-    databaseURL: "https://sollowfirebase-4f1da.firebaseio.com",
-    projectId: "sollowfirebase-4f1da",
-    storageBucket: "sollowfirebase-4f1da.appspot.com",
-    messagingSenderId: "660275076164",
-    appId: "1:660275076164:web:f129e3acd8003c6bf8b011",
-    measurementId: "G-SXHD4WNYRR"
+ firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
 
   oneSignal:{
-    appId: "50ecfea1-c0ba-495b-bf11-1052d652e122",
-    restApiKey: "NGMzNWE1NzQtMzI5Yi00MjEwLTlmNmYtODY2NDI2NjE0OWY3"
+    appId: "",
+    restApiKey: ""
   }
 };
